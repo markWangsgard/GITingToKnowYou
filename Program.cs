c1275a53a17ddf41a749b8233ce4a7e2a589f872
unknown <andrew.baumann@engineering.snow>
2026-01-16 10:59:35 -0700

    answered question

--- Program.cs
+++ Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Hello, World!");
 
 // Question 1: What is your favorite programming language and Why?
+
+// Andrew's response: I like html and css because it's a language I learned more by exploring.
